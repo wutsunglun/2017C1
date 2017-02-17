244b9661f87b534a9927e6d8b56969e8684bc09d
unknown <>
2017-02-17 16:50:47 +0800

    HOMEWORK
    I

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int X = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            label1.Text = (x + y).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int X = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            label1.Text = (x + y).ToString();
+        }
+    }
+}
